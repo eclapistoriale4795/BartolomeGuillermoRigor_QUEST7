0c276ac1b2e645ffbf08f8c3c9c287d5f306e250
NijiRaku Makulay <36956071+eclapistoriale4795@users.noreply.github.com>
2025-07-11 17:36:47 +0800

    Door has been created.

---
+++ Assets/Scripts/DoorManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    public Animator animator;
+    bool open;
+    float toggleCooldown = 1f;
+    float cooldownTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        open = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cooldownTimer >= 0)
+        {
+            cooldownTimer -= Time.deltaTime;
+        }
+        else
+        {
+            cooldownTimer = 0;
+        }
+        if (open)
+        {
+            animator.Play("DoorOpenIdle");
+        }
+        else { animator.Play("DoorCloseIdle"); }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                if (cooldownTimer == 0f)
+                {
+                    cooldownTimer = toggleCooldown;
+                    if (open) { open = false; animator.Play("DoorClosed"); Debug.Log("Closed"); }
+                    else { open = true; animator.Play("DoorOpened"); Debug.Log("Open"); }
+                }
+            }
+        }
+    }
+}
